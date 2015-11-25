2e0d31074326af604c552a928022336ead872f71
J. "Marahin" M <me@marahin.pl>
2015-11-26 00:07:58 +0100

    Get page title for shortened link
    Added gem 'mechanize' to obtain url's page title or filename (if it's a online-hosted file).

--- lib/shortener.rb
+++ lib/shortener.rb
@@ -17,9 +17,27 @@ class Gimbus
 
   def listen(m)
     urls = URI.extract(m.message, ["http", "https", "www."] )
-    short_urls = urls.map { |url| Gimbus.shorten(url) }.compact
+    short_urls = urls.map { |url| Gimbus.shorten(url) + " - " + get_page_title( url ) }.compact
     unless short_urls.empty?
       m.reply "➥ #{short_urls.join(", ")}"
+    end
+  end
+  
+  private
+  
+  def get_page_title( page_address )
+    page_obj = Mechanize.new( page_address )
+    if page_obj.respond_to?(:title)
+      # return title (if we can access it)
+      page_obj.title
+    else
+      # return filename if there's no page title
+      if page_obj.respond_to?(:filename)
+        page_obj.filename
+      else
+        # if we can't access anything - return homepage title
+        get_page_title( "http://" + page_obj.uri.host + "/" )
+      end
     end
   end
 end
