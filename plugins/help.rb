e65b29bd4f94558822530bba6a61616779e23109
J. "Marahin" M <me@marahin.pl>
2015-11-28 20:24:28 +0100

    Updating help.rb

--- plugins/help.rb
+++ plugins/help.rb
@@ -14,102 +14,103 @@ class Help
   match /help(.*)/i
 
   set :help, <<-EOF
-help
+[/msg] cinch help
   Post a short introduction and list available plugins.
-help <plugin>
+[/msg] cinch help <plugin>
   List all commands available in a plugin.
-help search <query>
+[/msg] cinch help search <query>
   Search all plugin’s commands and list all commands containing
   <query>.
-EOF
+  EOF
 
-def execute(msg, query)
-  query = query.strip.downcase
-  response = ""
+  def execute(msg, query)
+    query = query.strip.downcase
+    response = ""
 
-  # Act depending on the subcommand.
-  if query.empty?
-    response << @intro_message.strip << "\n"
-    response << "Available plugins:\n"
-    response << bot.config.plugins.plugins.map{|plugin| format_plugin_name(plugin)}.join(", ")
-    response << "\n'help <plugin>' for help on a specific plugin."
+    # Act depending on the subcommand.
+    if query.empty?
+      response << @intro_message.strip << "\n"
+      response << "Available plugins:\n"
+      response << bot.config.plugins.plugins.map{|plugin| format_plugin_name(plugin)}.join(", ")
+      response << "\n'help <plugin>' for help on a specific plugin."
 
-  # Help for a specific plugin
-  elsif plugin = @help.keys.find{|plugin| format_plugin_name(plugin) == query}
-    @help[plugin].keys.sort.each do |command|
-      response << format_command(command, @help[plugin][command], plugin)
+    # Help for a specific plugin
+    elsif plugin = @help.keys.find{|plugin| format_plugin_name(plugin) == query}
+      @help[plugin].keys.sort.each do |command|
+        response << format_command(command, @help[plugin][command], plugin)
+      end
+
+    # help search <...>
+    elsif query =~ /^search (.*)$/i
+      query2 = $1.strip
+      @help.each_pair do |plugin, hsh|
+        hsh.each_pair do |command, explanation|
+          response << format_command(command, explanation, plugin) if command.include?(query2)
+        end
+      end
+
+      # For plugins without help
+      response << "Sorry, no help available for the #{format_plugin_name(plugin)} plugin." if response.empty?
+
+    # Something we don't know what do do with
+    else
+      response << "Sorry, I cannot find '#{query}'."
     end
 
-  # help search <...>
-  elsif query =~ /^search (.*)$/i
-    query2 = $1.strip
-    @help.each_pair do |plugin, hsh|
-      hsh.each_pair do |command, explanation|
-        response << format_command(command, explanation, plugin) if command.include?(query2)
+    response << "Sorry, nothing found." if response.empty?
+    msg.reply(response)
+  end
+
+  # Called on startup. This method iterates the list of registered plugins
+  # and parses all their help messages, collecting them in the @help hash,
+  # which has a structure like this:
+  #
+  #   {Plugin => {"command" => "explanation"}}
+  #
+  # where +Plugin+ is the plugin’s class object. It also parses configuration
+  # options.
+  def on_connect(msg)
+    @help = {}
+
+    if config[:intro]
+      @intro_message = config[:intro] % bot.nick
+    else
+      @intro_message = "#{bot.nick} at your service."
+    end
+
+    bot.config.plugins.plugins.each do |plugin|
+      @help[plugin] = Hash.new{|h, k| h[k] = ""}
+      next unless plugin.help # Some plugins don't provide help
+      current_command = "<unparsable content>" # For not properly formatted help strings
+
+      plugin.help.lines.each do |line|
+        if line =~ /^\s+/
+          @help[plugin][current_command] << line.strip
+        else
+          current_command = line.strip.gsub(/cinch/i, bot.name)
+        end
       end
     end
-
-    # For plugins without help
-    response << "Sorry, no help available for the #{format_plugin_name(plugin)} plugin." if response.empty?
-
-  # Something we don't know what do do with
-  else
-    response << "Sorry, I cannot find '#{query}'."
   end
 
-  response << "Sorry, nothing found." if response.empty?
-  msg.reply(response)
-end
+  private
 
-# Called on startup. This method iterates the list of registered plugins
-# and parses all their help messages, collecting them in the @help hash,
-# which has a structure like this:
-#
-#   {Plugin => {"command" => "explanation"}}
-#
-# where +Plugin+ is the plugin’s class object. It also parses configuration
-# options.
-def on_connect(msg)
-  @help = {}
+  # Format the help for a single command in a nice, unicode mannor.
+  def format_command(command, explanation, plugin)
+    result = ""
 
-  if config[:intro]
-    @intro_message = config[:intro] % bot.nick
-  else
-    @intro_message = "#{bot.nick} at your service."
+    result << "┌─ " << command << " ─── Plugin: " << format_plugin_name(plugin) << " ─" << "\n"
+    result << explanation.lines.map(&:strip).join(" ").chars.each_slice(80).map(&:join).join("\n")
+    result << "\n" << "└ ─ ─ ─ ─ ─ ─ ─ ─\n"
+
+    result
   end
 
-  bot.config.plugins.plugins.each do |plugin|
-    @help[plugin] = Hash.new{|h, k| h[k] = ""}
-    next unless plugin.help # Some plugins don't provide help
-    current_command = "<unparsable content>" # For not properly formatted help strings
-
-    plugin.help.lines.each do |line|
-      if line =~ /^\s+/
-        @help[plugin][current_command] << line.strip
-      else
-        current_command = line.strip.gsub(/cinch/i, bot.name)
-      end
-    end
+  # Downcase the plugin name and clip it to the last component
+  # of the namespace, so it can be displayed in a user-friendly
+  # way.
+  def format_plugin_name(plugin)
+    plugin.to_s.split("::").last.downcase
   end
-end
 
-private
-
-# Format the help for a single command in a nice, unicode mannor.
-def format_command(command, explanation, plugin)
-  result = ""
-
-  result << "┌─ " << command << " ─── Plugin: " << format_plugin_name(plugin) << " ─" << "\n"
-  result << explanation.lines.map(&:strip).join(" ").chars.each_slice(80).map(&:join).join("\n")
-  result << "\n" << "└ ─ ─ ─ ─ ─ ─ ─ ─\n"
-
-  result
-end
-
-# Downcase the plugin name and clip it to the last component
-# of the namespace, so it can be displayed in a user-friendly
-# way.
-def format_plugin_name(plugin)
-  plugin.to_s.split("::").last.downcase
-end
 end
